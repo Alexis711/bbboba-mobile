d73dddb59eee8a5ad9313da31c0bcbaae193ce34
Alexis.Mendez.AwsCC <amendezp@celucenter.com>
2024-11-24 22:16:02 -0600

    Proyecto base
    Creacion de proyecto base y cambios en menu

---
+++ src/app/menu/menu-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MenuPage } from './menu.page';
+
+const routes: Routes = [
+  {
+    path: 'menu',
+    component: MenuPage,
+    children: [
+      {
+        path: 'ticket',
+        loadChildren: () => import('./ticket/ticket.module').then( m => m.TicketPageModule)
+      },
+      {
+        path: 'pedidos',
+        loadChildren: () => import('./pedidos/pedidos.module').then( m => m.PedidosPageModule)
+      },
+      {
+        path: 'ventas',
+        loadChildren: () => import('./ventas/ventas.module').then( m => m.VentasPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/menu/ticket',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/menu/ticket',
+    pathMatch: 'full'
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+})
+export class MenuPageRoutingModule {}
