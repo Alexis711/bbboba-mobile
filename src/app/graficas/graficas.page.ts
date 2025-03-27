49f4b1148d4c13e77b72a05bcb745414ea62f1da
Alexis.Mendez.AwsCC <amendezp@celucenter.com>
2025-03-26 19:31:41 -0600

    Integracion de la graficas

--- src/app/graficas/graficas.page.ts
+++ src/app/graficas/graficas.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Chart, PieController, ArcElement, Legend } from 'chart.js';
 
 @Component({
   selector: 'app-graficas',
   templateUrl: './graficas.page.html',
   styleUrls: ['./graficas.page.scss'],
 })
-export class GraficasPage implements OnInit {
-
+export class GraficasPage implements OnInit, AfterViewInit {
+  public grafica: any;
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  ngAfterViewInit(){
+      this.onGraficaGeneral();
+  }
+
+  onGraficaGeneral(){
+    Chart.register(PieController, ArcElement, Legend);
+    this.grafica = new Chart('graficaVentas', {
+      type: 'pie',
+      data: {
+        labels: 
+        [
+          'Frappes',
+          'Marquesitas',
+          'Desayunos'
+        ],
+        datasets: 
+        [
+          {
+            label: 'Graficas de Hoy',
+            data: [15, 9, 10],
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'], 
+          }
+        ],
+      }
+    });
+  }
 }
