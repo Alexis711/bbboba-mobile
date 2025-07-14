c27784994cc49b300096feece1106384edfa8f0c
¨Alexis.Mendez.AwsCC¨ <amendezp@celucenter.com>
2025-07-14 01:09:34 -0600

    desarrollo de la pagina de condiciones

--- src/app/condiciones/condiciones.page.ts
+++ src/app/condiciones/condiciones.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import DataTable from 'datatables.net-dt';
 
 @Component({
   selector: 'app-condiciones',
@@ -6,10 +7,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./condiciones.page.scss'],
 })
 export class CondicionesPage implements OnInit {
+  @ViewChild('tablaCondiciones', { static: false }) tablaDatos!: ElementRef
+  data = [
+    {
+      nombre: '2 Ingredientes',
+      producto: 'Marquesitas',
+      listaextras: 'Queso Crema, queso bola, ruffles',
+      costo: '55',
+    },
+    {
+      nombre: '4 Ingredientes',
+      producto: 'Marquesitas',
+      listaextras: 'Queso Crema, queso bola, ruffles',
+      costo: '80',
+    }
+  ];
+
+  isLoading: boolean = false;
+  searchTerm: string = '';
+  itemsPerPage: number = 10;
+  showSelector: boolean = false;
+  currentPage: number = 1;
+  totalRegistros: number = 100;
+  isPopoverLimiteAbierto = false;
+  eventoPopoverLimite: Event | null = null;
 
   constructor() { }
 
   ngOnInit() {
   }
+ngAfterViewInit() {
+    setTimeout(() => {
+      this.onTablaUsuarios();
+    }, 0);
+  }
 
+  private onTablaUsuarios() {
+    new DataTable('#tablaCondiciones', {
+      paging: false,
+      searching: false,
+      info: false,
+      autoWidth: false,
+      language: {
+        'emptyTable': 'No hay información',
+        'info': 'Mostrando _START_ a _END_ de _TOTAL_ Entradas',
+        'lengthMenu': 'Mostrar _MENU_ Entradas',
+        'loadingRecords': 'Cargando...',
+        'processing': 'Procesando...',
+        'zeroRecords': 'Sin registros encontrados',
+      },
+    });
+  }
+
+  onSearch() {
+    setTimeout(() => {
+      //onListDatos
+    }, 1500)
+  }
+
+  setItemsPerPage(value: number) {
+    this.itemsPerPage = value;
+    this.showSelector = false;
+    //this.onListAsignaciones('', '');
+  }
+
+  cambiarPagina(incremento: number) {
+    const nuevaPagina = this.currentPage + incremento;
+    if (nuevaPagina < 1) return;
+
+    this.irAPagina(nuevaPagina);
+  }
+
+  irAPagina(pagina: number) {
+    this.currentPage = pagina;
+    //this.onListAsignaciones('', '');
+  }
+
+  onInputChange() {
+    this.irAPagina(this.currentPage);
+  }
+
+  abrirPopoverLimite(ev: Event) {
+    this.eventoPopoverLimite = ev;
+    this.isPopoverLimiteAbierto = true;
+  }
 }
