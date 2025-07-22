e287f689e9ecc8ad938422ab0cde2df46a16781c
¨Alexis.Mendez.AwsCC¨ <amendezp@celucenter.com>
2025-07-21 22:19:28 -0600

    creacion de pagina extras

--- src/app/extras/extras.page.ts
+++ src/app/extras/extras.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import DataTable from 'datatables.net-dt';
 
 @Component({
   selector: 'app-extras',
@@ -6,10 +7,87 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./extras.page.scss'],
 })
 export class ExtrasPage implements OnInit {
+@ViewChild('tablaExtras', { static: false }) tablaDatos!: ElementRef
+  data = [
+    {
+      nombre: 'Leche Light',
+      productos: 'Frappe, Latte',
+      precio: '5',
+    },
+    {
+      nombre: 'Boba Explosiva',
+      productos: 'Frappe, Latte',
+      precio: '15',
+    }
+  ];
+
+  isLoading: boolean = false;
+  searchTerm: string = '';
+  itemsPerPage: number = 10;
+  showSelector: boolean = false;
+  currentPage: number = 1;
+  totalRegistros: number = 100;
+  isPopoverLimiteAbierto = false;
+  eventoPopoverLimite: Event | null = null;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.onTablaUsuarios();
+    }, 0);
+  }
+
+  private onTablaUsuarios() {
+    new DataTable('#tablaMovimientos', {
+      paging: false,
+      searching: false,
+      info: false,
+      autoWidth: false,
+      language: {
+        'emptyTable': 'No hay información',
+        'info': 'Mostrando _START_ a _END_ de _TOTAL_ Entradas',
+        'lengthMenu': 'Mostrar _MENU_ Entradas',
+        'loadingRecords': 'Cargando...',
+        'processing': 'Procesando...',
+        'zeroRecords': 'Sin registros encontrados',
+      },
+    });
+  }
+
+  onSearch() {
+    setTimeout(() => {
+      //onListDatos
+    }, 1500)
+  }
+
+  setItemsPerPage(value: number) {
+    this.itemsPerPage = value;
+    this.showSelector = false;
+    //this.onListAsignaciones('', '');
+  }
+
+  cambiarPagina(incremento: number) {
+    const nuevaPagina = this.currentPage + incremento;
+    if (nuevaPagina < 1) return;
+
+    this.irAPagina(nuevaPagina);
+  }
+
+  irAPagina(pagina: number) {
+    this.currentPage = pagina;
+    //this.onListAsignaciones('', '');
+  }
+
+  onInputChange() {
+    this.irAPagina(this.currentPage);
+  }
+
+  abrirPopoverLimite(ev: Event) {
+    this.eventoPopoverLimite = ev;
+    this.isPopoverLimiteAbierto = true;
+  }
 }
