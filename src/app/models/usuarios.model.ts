70c2dd3ab8d067db3b96e647a21bb00711ae2e95
¨Alexis.Mendez.AwsCC¨ <amendezp@celucenter.com>
2025-08-13 21:35:53 -0600

    modelo de usuario

--- src/app/models/usuarios.model.ts
+++ src/app/models/usuarios.model.ts
@@ -1,2 +1,11 @@
 export class Usuarios {
+    usu_id!: string;
+    usu_nom_usu!: string;
+    usu_nombres!: string;
+    usu_apellidos!: string;
+    usu_correo!: string;
+    usu_telefono!: string;
+    usu_clave!: string;
+    usu_estatus!: number;
+    usu_rol!: number;
 }
