6734fae02c64fea471fc72c716c009b5da881acd
¨Alexis.Mendez.AwsCC¨ <amendezp@celucenter.com>
2025-08-17 20:36:25 -0600

    metodo get usuarios completado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: 
@@ -15,6 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
 
   ],
   providers: 
--- src/app/usuarios/usuarios.page.ts
+++ src/app/usuarios/usuarios.page.ts
@@ -10,58 +10,14 @@ import { UsuariosService } from '../services/usuarios.service';
 })
 export class UsuariosPage implements OnInit {
   @ViewChild('tablaUsuarios', { static: false }) tablaDatos!: ElementRef
-  data: Usuarios[] = [];
-
-  /*columnas = [
-    { header: 'Id', field: 'usu_id', width: '7rem' },
-    { header: 'Usuario', field: 'usu_nom_usu',  direccion: 'desc', width: '7rem' },
-    { header: 'Nombre(s)', field: 'usu_nombres', width: '10rem' },
-    { header: 'Apellidos', field: 'usu_apellidos', width: '10rem' },
-    { header: 'Estatus', field: 'usu_estatus', width: '6rem' },
-    { header: 'Correo', field: 'usu_correo', width: '12rem' },
-    { header: 'Teléfono', field: 'usu_telefono', width: '5rem' },
-    { header: 'Rol', field: 'usu_rol', width: '6rem' },
-    {
-      header: 'Acciones',
-      field: 'acciones',
-      width: '10rem',
-      showButton: true,
-      showButtons: [
-        {
-          iconName: 'eye',
-          background: 'var(--light-neutral)',
-          color: 'var(--primary-color)',
-          clickButton: (item: any) => this.onVerUsuario(item),
-          access: 'auth/recharge',
-          accessType: 'write',
-          //isAdmin: true,
-        },
-        {
-          iconName: 'pencil',
-          background: 'var(--light-neutral)',
-          color: 'var(--warning-color)',
-          clickButton: (item: any) => this.onVerUsuario(item),
-          access: 'auth/recharge',
-          accessType: 'write',
-        },
-        {
-          iconName: 'trash',
-          background: 'var(--light-neutral)',
-          color: 'var(--danger-color)',
-          clickButton: (item: any) => this.onVerUsuario(item),
-          access: 'auth/recharge',
-          accessType: 'write',
-        }
-      ]
-    }
-  ];*/
-
+  dataUser: Usuarios[] = [];
   isLoading: boolean = false;
   searchTerm: string = '';
   itemsPerPage: number = 10;
   showSelector: boolean = false;
   currentPage: number = 1;
-  totalRegistros: number = 100;
+  totalPaginas: number = 1;
+  totalRegistros: number = 1;
   isPopoverLimiteAbierto = false;
   eventoPopoverLimite: Event | null = null;
 
@@ -80,41 +36,19 @@ export class UsuariosPage implements OnInit {
         console.log(resp);
         
         if (resp.status) {
-          this.data = resp.response;
+          this.dataUser = resp.response;
+          this.totalRegistros = resp.pagination.total;
+          this.totalPaginas = resp.pagination.totalPages;
         }
-        console.log('DATA',this.data);
+        console.log('DATA',this.dataUser);
         
       },
       error: (err: any) => {
-        this.data = [];
+        this.dataUser = [];
         console.log('Error',err);
         
       },
       
-    });
-  }
-
-  
-  ngAfterViewInit() {
-    setTimeout(() => {
-      this.onTablaUsuarios();
-    }, 0);
-  }
-
-  private onTablaUsuarios() {
-    new DataTable('#tablaUsuarios', {
-      paging: false,
-      searching: false,
-      info: false,
-      autoWidth: false,
-      language: {
-        'emptyTable': 'No hay información',
-        'info': 'Mostrando _START_ a _END_ de _TOTAL_ Entradas',
-        'lengthMenu': 'Mostrar _MENU_ Entradas',
-        'loadingRecords': 'Cargando...',
-        'processing': 'Procesando...',
-        'zeroRecords': 'Sin registros encontrados',
-      },
     });
   }
 
@@ -133,13 +67,12 @@ export class UsuariosPage implements OnInit {
   cambiarPagina(incremento: number) {
     const nuevaPagina = this.currentPage + incremento;
     if (nuevaPagina < 1) return;
-
     this.irAPagina(nuevaPagina);
   }
 
   irAPagina(pagina: number) {
     this.currentPage = pagina;
-    //this.onListAsignaciones('', '');
+    this.onListUser()
   }
 
   onInputChange() {
