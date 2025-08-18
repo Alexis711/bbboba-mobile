7e592281bd16328b5961fac83f32cfabc2f68ba3
¨Alexis.Mendez.AwsCC¨ <amendezp@celucenter.com>
2025-08-17 20:04:00 -0600

    services completo

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+  private url = environment.urlBase + 'usuarios/';
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUsers(payload: any) {
+    return this.http.get(this.url + 'todos', {
+      headers: this.headers,
+      params: payload,
+    });
+  }
 }
