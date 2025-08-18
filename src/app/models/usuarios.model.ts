f17bd77bcd67c5acfd8c670eb4f37e54156c6b67
¨Alexis.Mendez.AwsCC¨ <amendezp@celucenter.com>
2025-08-17 20:10:06 -0600

    modelo creado

--- src/app/models/usuarios.model.ts
+++ src/app/models/usuarios.model.ts
@@ -1,2 +1,15 @@
 export class Usuarios {
+    usu_id!: string;
+    usu_nom_usu!: string;
+    usu_nombres!: string;
+    usu_apellidos!: string;
+    usu_correo!: string;
+    usu_telefono!: string;
+    usu_clave!: string;
+    usu_estatus!: number;
+    usu_rol!: number;
+
+    constructor(init?: Partial<Usuarios>) {
+        Object.assign(this, init);
+    }
 }
