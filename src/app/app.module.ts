d24ea0d7f54c58ed06f78b37841e7e39aabbc31d
¨Alexis.Mendez.AwsCC¨ <amendezp@celucenter.com>
2025-08-21 01:41:47 -0600

    eliminacionde formularios generales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: 
@@ -18,8 +17,6 @@ import { FormsModule } from '@angular/forms';
     IonicModule.forRoot(), 
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-
   ],
   providers: 
   [
